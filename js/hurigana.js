aad33be95999a901162fbf177ae08a52b3ab9489
17hagiwara <17hagiwara@gmail.com>
2023-03-10 17:06:21 +0900

    3.10 add

--- js/hurigana.js
+++ js/hurigana.js
@@ -1,30 +1,29 @@
-// html上のテキストインプット上の情報を取得
-let tarea = document.querySelector('textarea');
-// idを参照する場合は#Id名と書く
-let findipt = document.querySelector('#findtxt');
-// htmlのIDrepiptを参照する
-let repipt = document.querySelector('#reptxt');
-// ボタンを参照する
-let btn = document.querySelector('button');
-//  Pを参照する
-let elem = document.querySelector('p');
+// // html上のテキストインプット上の情報を取得
+// let tarea = document.querySelector('textarea');
+// // idを参照する場合は#Id名と書く
+// let findipt = document.querySelector('#findtxt');
+// // htmlのIDrepiptを参照する
+// let repipt = document.querySelector('#reptxt');
+// // ボタンを参照する
+// let btn = document.querySelector('button');
+// //  Pを参照する
+// let elem = document.querySelector('p');
 
-btn.addEventListener('click', () => {
+// btn.addEventListener('click', () => {
 
-    // 置き換えたい文字
-    let findtxt = findipt.value;
-    // 置き換える
-    let reptxt = repipt.value;
+//     // 置き換えたい文字
+//     let findtxt = findipt.value;
+//     // 置き換える
+//     let reptxt = repipt.value;
 
-    // テキストエリア(tarea)
-    let tagtxt = tarea.value;
+//     // テキストエリア(tarea)
+//     let tagtxt = tarea.value;
 
-    findtxt = new RegExp(findtxt, 'g');
+//     findtxt = new RegExp(findtxt, 'g');
 
-    tagtxt = tagtxt.replace(findtxt, reptxt);
-    elem.innerText = tagtxt;
-});
-
+//     tagtxt = tagtxt.replace(findtxt, reptxt);
+//     elem.innerText = tagtxt;
+// });
 
 //アロー関数 //=()=>{}
 // let createMail = (recv, bill) => { //(「引数(パラメータ)」指定)
@@ -35,9 +34,31 @@ btn.addEventListener('click', () => {
 // };
 // createMail('萩原くん', '3000'); 　//作った関数を指定(パラメータの文字列);
 
+// let kakaku = prompt('価格を入力します');
+// console.log(parseInt(kakaku) + 80);
+
+// let moji = prompt('価格を入力します');
+// console.log(!isNaN(moji));
 
 
 
+// console.log((2 + 10) * 5)
 
+// let kakaku2 = 100;
+// let urine = kakaku2 * 1.08;
+// console.log(urine);
+
+// let moji3 = prompt('入力せよ');
+// if (!isNaN(moji3)) {
+//     console.log('文字です');
+// }
+
+
+let moji = prompt('入力せよ');
+if (!isNaN(moji)) {
+    console.log(parseInt(moji) + 80)
+
+    console.log('まだブロック内です')
+}
 
 
